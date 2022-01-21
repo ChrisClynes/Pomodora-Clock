948e3baead7c05f285a980fdf2b4162bda80b423
Chris Clynes <chrisclynes@live.com>
2022-01-20 23:26:30 -0500

    updateed visuals

--- index.js
+++ index.js
@@ -12,21 +12,36 @@ class App extends React.Component {
     render() {
         const {session, breakLength, sessionLength} = this.state;
         return (
-            <div id="container">
-                <div className="label-wrapper" id="break-label">Break Length
-                    <div className="grid-wrapper">
-                        <button type="button" id="break-increment" className="btn btn-primary incrementLeft"><span className="fa fa-arrow-up fa-2x"></span></button>
-                            <div className="incrementCenter" id="break-length">{breakLength}</div>
-                        <button id="break-decrement" className="btn btn-primary incrementRight"><span className="fa fa-arrow-down fa-2x"></span></button>
+            <div id="center-container">
+                <div id="clock-container">
+                    <div id="app-header">Pomodora Clock</div>
+                    <div className="timer-wrapper">
+                        <div className="session-wrapper" id="timer-label">Session
+                        <div id="time-left">{session}</div>
+                            <div className="grid-wrapper">
+                                <button id="session-play" className="btn btn-primary incrementLeft"><span className="fa fa-play fa-2x"></span></button>
+                                <button id="session-pause" className="btn btn-primary incrementCenter"><span className="fa fa-pause fa-2x"></span></button>
+                                <button id="reset" className="btn btn-primary incrementRight"><span className="fa fa-refresh fa-2x"></span></button>
+                            </div>
+                        </div>
                     </div>
-                </div>
-                <div className="label-wrapper" id="session-label">Session Length
-                    <div className="grid-wrapper">
-                        <button id="session-increment" className="btn btn-primary incrementLeft"><span className="fa fa-arrow-up fa-2x"></span></button>
-                            <div className="incrementCenter" id="session-length">{sessionLength}</div>
-                        <button id="session-decrement" className="btn btn-primary incrementRight"><span className="fa fa-arrow-down fa-2x"></span></button>
+                    <div className="controls-wrapper">
+                        <div className="label-wrapper" id="break-label">Break Length
+                            <div className="grid-wrapper">
+                                <button type="button" id="break-increment" className="btn btn-primary incrementLeft"><span className="fa fa-arrow-up fa-2x"></span></button>
+                                    <div className="incrementCenter" id="break-length">{breakLength}</div>
+                                <button id="break-decrement" className="btn btn-primary incrementRight"><span className="fa fa-arrow-down fa-2x"></span></button>
+                            </div>
+                        </div>
+                        <div className="label-wrapper" id="session-label">Session Length
+                            <div className="grid-wrapper">
+                                <button id="session-increment" className="btn btn-primary incrementLeft"><span className="fa fa-arrow-up fa-2x"></span></button>
+                                    <div className="incrementCenter" id="session-length">{sessionLength}</div>
+                                <button id="session-decrement" className="btn btn-primary incrementRight"><span className="fa fa-arrow-down fa-2x"></span></button>
+                            </div>
+                        </div>
                     </div>
-                </div>
+                </div>    
             </div>
         );
     }    
